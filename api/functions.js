a8e82d988684af982f972eae66edbb7290aed7cd
tongax12 <gastonstratta08@gmail.com>
2026-07-07 12:05:42 -0300

    fix: harcodeado de maxTokens

--- api/functions.js
+++ api/functions.js
@@ -30,7 +30,7 @@ export default async function handler(req, res) {
       config: {
         systemInstruction: system,
         temperature,
-        maxOutputTokens,
+        maxOutputTokens:400 ,
       },
     });
 
